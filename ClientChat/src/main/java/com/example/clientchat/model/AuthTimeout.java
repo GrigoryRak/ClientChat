3a596d038bb6520969fe99089b698ecab60eefea
Grigory.Rak <g.s.rak@yandex.ru>
2022-05-08 14:06:30 +0300

    final commit AuthTimeout Auth Form

--- ClientChat/src/main/java/com/example/clientchat/model/AuthTimeout.java
+++ ClientChat/src/main/java/com/example/clientchat/model/AuthTimeout.java
@@ -1,7 +1,6 @@
 package com.example.clientchat.model;
 
 import com.example.clientchat.ClientChat;
-import com.example.clientchat.controllers.AuthController;
 import com.example.clientchat.dialogs.Dialogs;
 import javafx.application.Platform;
 
@@ -13,25 +12,20 @@ public class AuthTimeout extends TimerTask {
     @Override
     public void run() {
         System.out.println("Timer timeoutTask started at: " + new Date());
-
-//        AuthController authController = new AuthController();
-//        authController.initializeMessageHandler();
-
-//        if (!authController.isConnectedToServer()) {
         timeoutTask();
         Platform.runLater(() -> {
-            Dialogs.AuthTimeout.AUTH_TIMEOUT.show();
-            ClientChat.getInstance().getAuthStage().close();
-            ClientChat.getInstance().getChatStage().close();
+            if (!Network.getInstance().isConnected()) {
+                Dialogs.AuthTimeout.AUTH_TIMEOUT.show();
+                ClientChat.getInstance().getAuthStage().close();
+                ClientChat.getInstance().getChatStage().close();
+                System.out.println("Timer timeoutTask finished at: " + new Date());
+            }
         });
-        System.out.println("Timer timeoutTask finished at: " + new Date());
-//        }
     }
 
     private void timeoutTask() {
         try {
-            // На выполнение задачи требуется 10 секунд
-            Thread.sleep(10000);
+            Thread.sleep(120000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
